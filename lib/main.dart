41d2656f1cd6d0df252b6398ed736f27781531fc
Reinier Garcia Ramos <reymillenium@gmail.com>
2023-06-13 00:45:07 -0400

    Shell is ready

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(title: 'Tok-Tik'),
     );
   }
 }
