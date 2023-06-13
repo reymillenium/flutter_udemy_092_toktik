49e8057d6468422d1dcde2eb321d5342a8cb0f55
Reinier Garcia Ramos <reymillenium@gmail.com>
2023-06-12 21:18:37 -0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_udemy_092_toktik/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_udemy_092_toktik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_udemy_092_toktik/config/theme/app_theme.dart';
+import 'package:flutter_udemy_092_toktik/presentation/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme().getTheme(),
+      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
