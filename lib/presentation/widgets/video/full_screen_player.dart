1851c0adae209cd76ede9f4639f8926bab61de30
Reinier Garcia Ramos <reymillenium@gmail.com>
2023-06-20 11:25:46 -0400

    Converted to statefull

--- lib/presentation/widgets/video/full_screen_player.dart
+++ lib/presentation/widgets/video/full_screen_player.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
   const FullScreenPlayer({super.key, required this.videoUrl, required this.caption});
 
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
