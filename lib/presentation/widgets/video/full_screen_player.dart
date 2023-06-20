b1c185766038afa70468553275736524eb0a6c78
Reinier Garcia Ramos <reymillenium@gmail.com>
2023-06-20 11:46:04 -0400

    Installed the get package. Add some config for the video player

--- lib/presentation/widgets/video/full_screen_player.dart
+++ lib/presentation/widgets/video/full_screen_player.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
+import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
@@ -11,8 +13,58 @@ class FullScreenPlayer extends StatefulWidget {
 }
 
 class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+
+    _controller = widget.videoUrl.isURL
+        ? VideoPlayerController.network(
+            'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+          )
+        : VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the VideoPlayerController has finished initialization, use
+          // the data it provides to limit the aspect ratio of the video.
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            // Use the VideoPlayer widget to display the video.
+            child: VideoPlayer(_controller),
+          );
+        } else {
+          // If the VideoPlayerController is still initializing, show a
+          // loading spinner.
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
